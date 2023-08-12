5761e36cf018148ee7ed27d46cedda2121147062
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-08-12 15:20:37 +0600

    Video #8

---
+++ src/8/08_01.test.ts
@@ -0,0 +1,26 @@
+import {UsersType} from "./08_01";
+
+let users: UsersType
+
+beforeEach(()=> {
+    users = {
+        "101": {id: 101, name: "Dimych"},
+        "323232": {id: 323232, name: "Sveta"},
+        "1212": {id: 1212, name: "Valera}"},
+        "1": {id: 1, name: "Victor"}
+    }
+})
+
+test("should update corresponding user", () => {
+    users["1"].name = "Ekaterina"
+
+    expect(users['1'].name).toBe("Ekaterina")
+    expect(users["1"]).toEqual({id: 1, name: "Ekaterina"})
+})
+
+test("should delete corresponding user", () => {
+    users["1"].name = "Ekaterina"
+
+    expect(users['1'].name).toBe(undefined)
+
+})
---
+++ src/8/08_01.ts
@@ -0,0 +1,26 @@
+export const userObj = {
+    "0": "Dimych",
+    "1": "Sveta",
+    "2": "Valera",
+    "3": "Victor"
+}
+
+export type UsersType = {
+    [key: string]: {id: number, name: string}
+}
+
+export const users = {
+    "101": {id: 101, name: "Dimych"},
+    "323232": {id: 323232, name: "Sveta"},
+    "1212": {id: 1212, name: "Valera}"},
+    "1": {id: 1, name: "Victor"}
+}
+
+
+
+export const userArray = [
+    {id: 101, name: "Dimych"},
+    {id: 323232, name: "Sveta"},
+    {id: 1212, name: "Valera}"},
+    {id: 1, name: "Victor"}
+]
