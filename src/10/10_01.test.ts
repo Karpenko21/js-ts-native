26d362c6ade8298760c14a56838d0ab1b1c6bf84
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-08-13 00:23:45 +0600

    Video #8 final

--- src/10/10_01.test.ts
+++ src/10/10_01.test.ts
@@ -1,10 +1,18 @@
-import {makeHairstyle, moveUser, upgradeUserLaptop, UserType, UserWithLaptop} from "./10_01";
-
+import {
+    addBook, CompaniesType, deleteBook,
+    makeHairstyle,
+    moveUser,
+    moveUserToOtherHouse, updateBook, updateCompany, updateCompany2,
+    upgradeUserLaptop,
+    UserType,
+    UserWithBooksType, UserWithCompaniesType,
+    UserWithLaptopType
+} from "./10_01";
 
 
 test("reference type test", () => {
 
-       let user: UserType = {
+    let user: UserType = {
         name: "Dimych",
         hair: 32,
         address: {
@@ -21,12 +29,12 @@ test("reference type test", () => {
 
 test("change address test", () => {
 
-    let user: UserWithLaptop = {
+    let user: UserWithLaptopType = {
         name: "Dimych",
         hair: 32,
         address: {
             city: "Minsk",
-            house:12
+            house: 12
         },
         laptop: {
             title: "Zenbook"
@@ -43,12 +51,12 @@ test("change address test", () => {
 
 test("upgrade user laptop test", () => {
 
-    let user: UserWithLaptop = {
+    let user: UserWithLaptopType = {
         name: "Dimych",
         hair: 32,
         address: {
             city: "Minsk",
-            house:12
+            house: 12
         },
         laptop: {
             title: "Zenbook"
@@ -60,5 +68,142 @@ test("upgrade user laptop test", () => {
     expect(newUser).not.toBe(user)
     expect(newUser.address).toBe(user.address)
     expect(newUser.laptop).not.toBe(user.laptop)
+    expect(user.laptop.title).toBe("Zenbook")
     expect(newUser.laptop.title).toBe("macbook")
+})
+
+test("change house test", () => {
+
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: "Dimych",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: "Zenbook"
+        },
+        books: ["JS", "HTML", "React", "css"]
+    }
+
+    let newUser = moveUserToOtherHouse(user, 105)
+
+    expect(newUser).not.toBe(user)
+    expect(newUser.address).not.toBe(user.address)
+    expect(newUser.laptop).toBe(user.laptop)
+    expect(user.address.house).toBe(12)
+    expect(newUser.address.house).toBe(105)
+})
+
+test("add new book test", () => {
+
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: "Dimych",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: "Zenbook"
+        },
+        books: ["JS", "HTML", "React", "css"]
+    }
+
+    let newUser = addBook(user, "ts")
+
+    expect(newUser).not.toBe(user)
+    expect(newUser.address).toBe(user.address)
+    expect(newUser.laptop).toBe(user.laptop)
+    expect(newUser.books.length).toBe(5)
+    expect(newUser.books[4]).toBe("ts")
+})
+
+test("update book test", () => {
+
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: "Dimych",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: "Zenbook"
+        },
+        books: ["JS", "HTML", "React", "css"]
+    }
+
+    let newUser = updateBook(user, "JS", "ts")
+
+    expect(newUser).not.toBe(user)
+    expect(newUser.books).not.toBe(user.books)
+    expect(newUser.address).toBe(user.address)
+    expect(newUser.laptop).toBe(user.laptop)
+    expect(newUser.books[0]).toBe("ts")
+})
+
+test("delete book test", () => {
+
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: "Dimych",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: "Zenbook"
+        },
+        books: ["JS", "HTML", "React", "css"]
+    }
+
+    let newUser = deleteBook(user, "JS")
+
+    expect(newUser).not.toBe(user)
+    expect(newUser.books).not.toBe(user.books)
+    expect(newUser.address).toBe(user.address)
+    expect(newUser.laptop).toBe(user.laptop)
+    expect(newUser.books.length).toBe(3)
+    expect(newUser.books[0]).toBe("HTML")
+})
+
+test("update company name test", () => {
+
+    let user: UserWithLaptopType & UserWithCompaniesType = {
+        name: "Dimych",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: "Zenbook"
+        },
+        companies: [{id: 1, title: "Епам"},
+            {id: 2, title: "IT-Incubator"}]
+    }
+
+    let newUser = updateCompany(user, 1, "EPAM") as UserWithLaptopType & UserWithCompaniesType
+
+    expect(newUser).not.toBe(user)
+    expect(newUser.address).toBe(user.address)
+    expect(newUser.laptop).toBe(user.laptop)
+    expect(newUser.companies[0].title).toBe("EPAM")
+    expect(newUser.companies).not.toBe(user.companies)
+})
+
+test("update company test", () => {
+
+    let companies: CompaniesType = {
+        "Dimych": [{id: 1, title: "Епам"},
+            {id: 2, title: "IT-Incubator"}],
+        "Victor": [{id: 2, title: "IT-Incubator"}]
+    }
+
+    let newCompanies = updateCompany2(companies, "Dimych", 1, "EPAM")
+    expect(newCompanies["Dimych"]).not.toBe(companies["Dimych"])
+    expect(newCompanies["Victor"]).toBe(companies["Victor"])
+    expect(newCompanies["Dimych"][0].title).toBe("EPAM")
 })
--- src/10/10_01.ts
+++ src/10/10_01.ts
@@ -1,27 +1,43 @@
 export type UserType = {
     name: string
     hair: number
-    address: {city: string, house?: number}
+    address: { city: string, house?: number }
 }
 
 export type LaptopType = {
     title: string
 }
 
-export type UserWithLaptop = UserType & {
+export type UserWithLaptopType = UserType & {
     laptop: LaptopType
 }
 
+export type UserWithBooksType = UserType & {
+    books: Array<string>
+}
+
+export type UserWithCompaniesType = UserType & {
+    companies: Array<CompanyType>
+}
+
+export type CompaniesType = {
+    [key: string]: CompanyType[]
+}
+
+export type CompanyType = {
+    id: number
+    title: string
+}
+
 export function makeHairstyle(u: UserType, power: number) {
-    const copy = {
+    return {
         ...u,
-        hair: u.hair/power
+        hair: u.hair / power
     }
-    return copy
 }
 
 
-export function moveUser(u: UserWithLaptop, city: string) {
+export function moveUser(u: UserWithLaptopType, city: string) {
     return {
         ...u,
         address: {
@@ -31,7 +47,7 @@ export function moveUser(u: UserWithLaptop, city: string) {
 }
 
 
-export function upgradeUserLaptop(u: UserWithLaptop, laptop: string) {
+export function upgradeUserLaptop(u: UserWithLaptopType, laptop: string) {
     return {
         ...u,
         laptop: {
@@ -39,3 +55,53 @@ export function upgradeUserLaptop(u: UserWithLaptop, laptop: string) {
         }
     }
 }
+
+export function moveUserToOtherHouse(u: UserWithLaptopType & UserWithBooksType, house: number) {
+    return {
+        ...u,
+        address: {
+            ...u.address, house
+        }
+    }
+}
+
+
+export function addBook(u: UserWithLaptopType & UserWithBooksType, newBook: string) {
+    return {
+        ...u,
+        books: [
+            ...u.books, newBook
+        ]
+    }
+}
+
+export const updateBook = (u: UserWithLaptopType & UserWithBooksType,
+                           oldBook: string,
+                           newBook: string) => ({
+    ...u,
+    books: u.books.map(b => b === oldBook ? newBook : b)
+})
+
+export const deleteBook = (u: UserWithLaptopType & UserWithBooksType, book: string) => (
+    {
+        ...u,
+        books: u.books.filter(b => b !== book)
+    })
+
+
+export const updateCompany = (u: UserWithCompaniesType,
+                              id: number,
+                              newCompanyTitle: string) => ({
+    ...u,
+    companies: u.companies.map(c => c.id === id ? {...c, title: newCompanyTitle} : c)
+})
+
+
+export const updateCompany2 = (companies: CompaniesType,
+                              userName: string,
+                              companyID: number,
+                              newTitle: string) => ({
+   ...companies,
+    [userName]: companies[userName].map(c => c.id === companyID ? {...c, title: newTitle} : c)
+})
+
